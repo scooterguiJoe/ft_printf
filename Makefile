88f51548a322065b58997670e8d27c4f88e9fdd2
scooterguiJoe <scootergui73@gmail.com>
2024-11-27 17:29:26 +0000

    2ndday

--- Makefile
+++ Makefile
@@ -6,7 +6,29 @@
 #    By: guvascon <guvascon@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/26 13:20:05 by guvascon          #+#    #+#              #
-#    Updated: 2024/11/26 13:20:06 by guvascon         ###   ########.fr        #
+#    Updated: 2024/11/27 16:12:49 by guvascon         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+SRCS = ft_print.c ft_putchar.c ft_putstr.c ft_putnbr.c ft_putnbr_base.Created
+
+OBJS = $(SRCS: .c=.o)
+
+CC = cc
+RM = rm -f
+CFLAGS = -Wall -Wextra -Werror
+
+Name = ft_printf.a
+
+all : $(NAME)
+	ar rcs $(NAME) $(OBJS)
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(OBJS)
+
+re: fclean $(NAME)
+
+.PHONY: all clean fclean re
--- ft_print_format.c
+++ ft_print_format.c
@@ -1,36 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_print_format.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: guvascon <guvascon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/26 13:20:10 by guvascon          #+#    #+#             */
-/*   Updated: 2024/11/27 16:23:10 by guvascon         ###   ########.fr       */
+/*   Created: 2024/11/27 13:43:57 by guvascon          #+#    #+#             */
+/*   Updated: 2024/11/27 15:12:21 by guvascon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "ft_printf"
 
-int	ft_printf(const char *format, ...)
+static int	ft_print_format(char special, va_list *args)
 {
-	va_list	args;
-	int	i;
 	int	count;
 
-	i = 0;
 	count = 0;
-	if (!format)
-		return (0);
-	va_start(args, format);
-	if (format == '%')
-	{
-		i++;
-		count += ft_print_format(format[i], args);
-	}
-	else
-		count += write(1, format[i], 1);
-	i++;
-	va_end(args);
-	return (count);
+	if (special == 'c')
+		count += ft_putchar(va_arg(*args, int));
+	else if (special == 's')
+		count += ft_putstr();
+	else if (special == 'p')
+		count +=
+	else if (special == 'd')
+
+	else if (special == 'i')
+
+	else if (special == 'u')
+
+	else if (special == 'x')
+	
+	else if (special == 'X')
+
+	else if (special == '%')
+		count += ft_putchar(special);
 }
