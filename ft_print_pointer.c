2d6253bb915b55633b299c0cbba9fa56a477b1e2
scooterguiJoe <scootergui73@gmail.com>
2024-11-28 18:16:53 +0000

    3rddday

---
+++ ft_print_pointer.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_pointer.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guvascon <guvascon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 18:10:41 by guvascon          #+#    #+#             */
+/*   Updated: 2024/11/28 18:16:02 by guvascon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stdio.h>
+
+int main()
+{
+   // char *i = "asdasdas";
+    printf("%p\n", NULL);
+}
+
+//quando for Null escrever nil
+//imprimir 0x
+//passar unsigned putnbrbase
